8846b92fb14d133e945eb46ee4b2380081a03394
AMAN KUMAR JHA <87240200+leaderamanjha@users.noreply.github.com>
2022-10-13 01:10:28 +0530

    Delete Project1 directory

--- Project1/front-script.js
+++
@@ -1,9 +0,0 @@
-const profile = document.querySelector('nav .profile');
-const imgProfile = profile.querySelector('img');
-const dropdownProfile = profile.querySelector('.profile-link');
-
-imgProfile.addEventListener('click', function() {
-    dropdownProfile.classList.toggle('show');
-})
-
-
